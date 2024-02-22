ab256b0044487264b843a4c5442b68c5bd900e4d
geno <yvesgeno@outlook.com>
2024-02-22 23:03:48 +0200

    task 11 correction

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -36,17 +36,13 @@ def register_user() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """POST /sessions
-    JSON body:
-      - email: string
-      - password: string
-    Return: {"email": email, "message": "logged in"}
+    Return:
+        - JSON payload of the form containing login info.
     """
-    email = request.form.get("email")
-    password = request.form.get("password")
     auth = Auth()
+    email, password = request.form.get("email"), request.form.get("password")
     if not auth.valid_login(email, password):
         abort(401)
     session_id = auth.create_session(email)
