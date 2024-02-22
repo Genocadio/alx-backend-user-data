1e40e6c802b48db447af75590f8c97caf4b71fd4
geno <yvesgeno@outlook.com>
2024-02-22 07:36:46 +0200

    task 1 corection

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,11 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        try:
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception as e:
+            print(f"Error adding user to database: {e}")
+            self._session.rollback()
+            raise
+        return new_user
