ac53a28a62efad8359727c4e720afebcbcf13532
geno <yvesgeno@outlook.com>
2024-02-22 17:00:56 +0200

    task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,14 @@ class Auth:
             pass
         hashed_password = _hash_password(password)
         return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        method to check if a user is valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
